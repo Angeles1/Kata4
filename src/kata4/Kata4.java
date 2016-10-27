9f429f75a16a943b2178fbf1fb641b8172491261
usuario <usuario@10.22.143.58>
2016-10-27 11:01:58 +0100

    Kata 4 v2

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -5,10 +5,19 @@ import java.io.File;
 public class Kata4 {
 
     public static void main(String[] args) {
-        File file = new File("c:\\"); //linux "c:/"
-        String [] names = file.list(); //guarda el listado y directorio de fichero
-        for (String name : names) {
-            System.out.println(name);
+        File file = new File("c:\\Users\\usuario"); //linux "c:/"
+        print(file.listFiles(),""); //método recursivo
+    }
+
+    public static void print(File[] listFiles, String string) {
+        if(listFiles ==null) return;
+        for (File file : listFiles) {
+            System.out.println(string + (file.isDirectory()?
+                    "+" : "-") + file.getName());
+            
+            if(!file.isDirectory() || file.isHidden()) continue;
+            print(file.listFiles(),"  ");
         }
     }
+
 }
